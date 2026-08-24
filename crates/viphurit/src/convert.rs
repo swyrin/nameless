1978d9a093402b55d64502caa2acc9e46b31dad4
Tien Dat Pham <hello@swyrin.me>
2026-08-24 16:45:33 +0700

    use `aho-corasick` crate for string replacement.

--- crates/viphurit/src/convert.rs
+++ crates/viphurit/src/convert.rs
@@ -1,19 +1,17 @@
 use crate::dictionary;
+use aho_corasick::AhoCorasick;
 
 #[must_use]
 pub fn make_viphurit(input: impl ToString) -> String {
     let input = input.to_string();
-    let words: Vec<&str> = input.split_whitespace().collect();
+
+    let patterns = dictionary::VIPHURIT.keys().collect::<Vec<_>>();
+    let replaces = dictionary::VIPHURIT.values().collect::<Vec<_>>();
+
+    let ac = AhoCorasick::new(patterns).unwrap();
 
     // https://discord.com/channels/1025394048204275732/1099318720259690599/1535290571390984222
-    let forbidden_knowledge = dictionary::VIPHURIT.clone();
-
-    let words_fixed: Vec<&str> = words
-        .iter()
-        .map(|word| *forbidden_knowledge.get(word).unwrap_or(word))
-        .collect();
-
-    words_fixed.join(" ")
+    ac.replace_all(&input, &replaces)
 }
 
 #[cfg(test)]
